1de6cc2ed0e5eccac6d45ac4f7bac6c5cd0280d9
Philip <philip@Philips-MacBook-Pro.local>
2025-11-23 19:36:27 +0100

    wip

--- VolatilityManager.js
+++ VolatilityManager.js
@@ -183,5 +183,5 @@ export const VolatilityManager = {
   getRealizedVolatility,
   getVolRegimeRatio,
   backfillHistory,
-  getPriceHistory  // NEW: Added for drift estimation
+  getPriceHistory
 };
